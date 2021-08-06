1544aebe04cf2d8d133430866f09109bff2c9450
Johannes <johannesduelberg@web.de>
2021-08-06 12:43:44 +0200

    fresh install

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-1ik70l(p7ylwj0^$aj1$aa*95b6y%-0_%ds-&u*1o0=o9rg1#6'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
