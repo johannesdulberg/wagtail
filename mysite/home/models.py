c01c68d2d880f4c3f25232d71223d42665cd3f25
Johannes <johannesduelberg@web.de>
2021-08-06 13:24:27 +0200

    first Fields

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 from wagtail.core.models import Page
 
+from wagtail.admin.edit_handlers import FieldPanel
+
 
 class HomePage(Page):
-    pass
+    templates="home/home_page.html"
+
+    banner_title = models.CharField(max_length=100,blank=False,null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title")
+    ]
+    
+    class Meta:
+        verbose_name = "NAME"
+        verbose_name_plural = "Plural name"
+
